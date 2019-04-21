955472d55170d97e39df3eeeae69ca797d8b1fd4
greedyshardul <aeershardul@gmail.com>
2019-04-21 12:24:26 +0530

    user add and delete in firestore working
    work on app now

--- firebase_functions/functions/src/index.ts
+++ firebase_functions/functions/src/index.ts
@@ -2,11 +2,48 @@ import * as functions from 'firebase-functions';
 import * as firebaseHelper from 'firebase-functions-helper';
 import * as admin from 'firebase-admin';
 admin.initializeApp();
+const usersCollection = 'users';
+const db=admin.firestore();
 exports.addGuardian = functions.auth.user().onCreate((user) => {
-    const name=user.displayName;
+    const email=user.email;
     //add name in firestore
-    const usersCollection = 'users';
-    const db=admin.firestore();
-    firebaseHelper.firestore
-        .createNewDocument(db, usersCollection, "mike");
+    
+    console.log("user "+email);
+    var myDoc = {
+        Email: email,
+        phone: null
+
+    };
+    if(firebaseHelper.firestore
+        .createNewDocument(db, usersCollection, myDoc)){
+            console.log(email+" added successfuly");
+            return 1;
+        }
+    return console.log("failed to write "+email);
+    return 1;
+  });
+  exports.removeGuardian=functions.auth.user().onDelete((user)=>{
+      //var docId;
+      //var usersRef=db.collection(usersCollection);
+      //var docQuery=usersRef.where("Email","==",user.email);
+      db.collection("users").where('Email', '==', user.email).get()
+          .then(function (querySnapshot) {
+              // Once we get the results, begin a batch
+              var batch = db.batch();
+
+              querySnapshot.forEach(function (doc) {
+                  // For each doc, add a delete operation to the batch
+                  batch.delete(doc.ref);
+              });
+
+              // Commit the batch
+              return batch.commit();
+          }).then(function () {
+              console.log("delete successful");
+              return 1;
+              // Delete completed!
+              // ...
+          }); 
+          console.log("delete fail");
+          return 0;
   });
